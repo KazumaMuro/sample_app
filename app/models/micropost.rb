8f9c69892affbdf7c6888e39d5fa198f646fda9c
EC2 Default User <ec2-user@ip-172-31-36-37.us-east-2.compute.internal>
2018-06-30 08:05:12 +0000

    Finish image upload my version

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,8 +1,9 @@
 class Micropost < ActiveRecord::Base
   belongs_to :user
   default_scope -> { order('created_at DESC') }
-  validates :content, presence: true, length: { maximum: 140 }
+  validates :content, presence: true, length: { maximum: 140 }  
   validates :user_id, presence: true
+  mount_uploader :image, ImageUploader
 
   def self.from_users_followed_by(user)
     followed_user_ids = "SELECT followed_id FROM relationships
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,6 @@ class User < ActiveRecord::Base
   has_many :followers, through: :reverse_relationships, source: :follower
 
 
-
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
